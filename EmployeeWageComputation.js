8554b2c27ce73a9a2232f6f372bfc250e0889964
Sai Methish <Sai.Methish@godigit.com>
2024-07-23 15:35:56 +0530

    added uc3

--- EmployeeWageComputation.js
+++ EmployeeWageComputation.js
@@ -1,6 +1,7 @@
 const employee={
     id:Math.floor(Math.random()*10),
     name:"emp1",
+    partTime:Math.random()>=0.5?1:0,
 };
 
 const checkAttendance=(id)=>{
@@ -12,9 +13,13 @@ const checkAttendance=(id)=>{
 }
 
 const calculateWage=()=>{
+    let wagePerHr,hrs;
+    wagePerHr=20;
     if(checkAttendance(employee.id)==="present"){
-        let wagePerHr=20;
-        let hrs=8;
+        if(employee.partTime==1){
+            hrs=4;
+        }
+        else hrs=8;
         return wagePerHr*hrs;
     }
     return "employee is absent";
